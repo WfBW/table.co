f35842776cf15ce1dc0226e1035fb5f1eba96d03
ded-maksim <36963568+ded-maksim@users.noreply.github.com>
2018-06-05 17:49:34 +0300

    Вывод проектов

--- JS/index.js
+++ JS/index.js
@@ -12,7 +12,10 @@ document.addEventListener("DOMContentLoaded", function () {
         login: null,
         password: null
     };
-
+    
+    // список проектов
+    var projectList;
+    
     ////////////////////////////
     //   служебные функции   //
     function showInputsError(text, time) {
@@ -23,31 +26,42 @@ document.addEventListener("DOMContentLoaded", function () {
         setTimeout(function () {
             alertBlock.style.visibility = "hidden";
         }, time * 1000);
-    }
+    };
     
-//    function ajax(metod, link, asink, data, callback) {
-//        var xhr = new XMLHttpRequest();
-//        xhr.open(metod, link, asink);
-//        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-//        xhr.onreadystatechange = function () {
-//            loadingBlock.style.display = "block";
-//            if (xhr.readyState == 4) {
-//                loadingBlock.style.display = "none";
-//                if (xhr.status == 200) {
-//                    callback(xhr.responseText);
-//                } else {
-//                    showInputsError("Ошибка загрузки...", 5);
-//                }
-//            }
-//        } xhr.send(data);
-//    };
+    function proj(idProj, nameProj, tzProj, dateDead, idUser, idOrg, imp) {
+        this.idProj = idProj;
+        this.nameProj = nameProj;
+        this.tzProj = nameProj;
+        this.dateDead = dateDead;
+        this.idUser = idUser;
+        this.idOrg = idOrg;
+        this.imp = imp;
+    };
+    
+    function ajax(metod, link, asink, data, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(metod, link, asink);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function () {
+            loadingBlock.style.display = "block";
+            if (xhr.readyState == 4) {
+                loadingBlock.style.display = "none";
+                if (xhr.status == 200) {
+                    callback(xhr.responseText);
+                } else {
+                    showInputsError("Ошибка загрузки...", 5);
+                }
+            }
+        }
+        xhr.send(data);
+    };
     //  конец служебных ф-й  //
     //////////////////////////
     
      //////////////////////////
     //         ФУНКЦИИ      //
     // открытие главного окна
-    function mainHtml () {
+    function mainHtml() {
         // блок в index.html
         var xhr = new XMLHttpRequest();
         xhr.open("get", "../Pages/main.html");
@@ -55,45 +69,143 @@ document.addEventListener("DOMContentLoaded", function () {
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4 && xhr.status == 200) {
                 mainView.innerHTML = xhr.responseText;
-                loadingBlock.style.display = "none";
                 
+                var mainProjectBlock = document.querySelector(".projectList");
+                
+                // создание элемекнта списка проектов
+                function projItem (container, projId) {
+                    container.innerHTML +=
+                    "<div class=\"block-content\">"+
+                        "<div class=\"title-block\">"+
+                            "<img class=\"proj-mark\" src=\"../IMG/mark.svg\">"+
+                            "<span class=\"caption\">"+ projId.name +"</span>"+
+                            "<hr>"+
+                            "<span class=\"name-company text14gray\">"+ projId.org +"</span>"+
+                        "</div>"+
+                        "<div class=\"deadline\">"+
+                            "<span class=\"caption\">Дедлайн:</span>"+
+                            "<span class=\"dead-date\">"+ projId.date +"</span>"+
+                        "</div>"+
+//                        "<div class=\"manager\">"+
+//                            "Менеджер проекта: <span class=\"manager-name\">Ефимов Пётр</span>"+
+//                        "</div>"+
+                        "<div class=\"tz\">"+
+                            "<span class=\"caption\">Техническое задание:</span>"+
+                            "<p class=\"tz-text\">"+ projId.tz +"</p>"+
+                        "</div>"+
+                        "<button class=\"btn-content\" id=\"proj"+ projId.id_proj +"\">Подробнее...</button>"+
+                    "</div>";
+                    document.querySelector("#proj"+projId.id_proj).onclick = function(){
+                        projMore(projId.id_proj, projId);  
+                    };
+                    alert(document.querySelector("#proj"+projId.id_proj).onclick);
+                };
+                
+                //подробнее о проекте
+                function projMore (idProj, proj) {
+                    var
+                        titleProj = document.querySelector("#mainProjMoreTitle"),
+                        companyProj = document.querySelector("#mainNameCompany"),
+                        deadProj = document.querySelector("#mainProjMoreDead"),
+                        tzProj = document.querySelector("#mainProjMoreTZ"),
+                        complTask = document.querySelector("#mainTaskComplit"),
+                        countTask = document.querySelector("#mainTaskCount"),
+                        mainTaskList = document.querySelector("#mainProjMoreList");
+                    
+                    titleProj.innerHTML = proj.name;
+                    companyProj.innerHTML = proj.org;
+                    deadProj.innerHTML = proj.date;
+                    tzProj.innerHTML = proj.tz;
+                    
+                    mainProjectBlock.style.display = "none";
+                    document.querySelector(".main-project-more").style.display = "block";
+                }
+                
+                //начало ajax получение проектов
+                ajax("POST", "http://tableco.ad-best.ru/php/proj/getProjData.php", false, 
+                     "login="+user.login+"&password="+user.password, function(response) {
+                    // ответ сервера в формате JSON
+                    projectList = JSON.parse(response);
+                    
+                    //контейнер с проектами
+                    var myProj = document.querySelector(".myProj");
+                    //контейнер с управлением
+                    var myProjControl = document.querySelector(".myProjControl");
+                    
+                    var caruselHuakin = document.querySelector(".carusel-huakin");
+                    
+                    for(var i=0; i < projectList[0].length; i++) {
+                        projItem(myProj, projectList[0][i]);
+                        caruselHuakin.style.width = (caruselHuakin.offsetWidth + 361) + "px";
+                    }
+                    
+                    for(var i=0; i < projectList[1].length; i++) {
+                        projItem(myProjControl, projectList[1][i]);
+                        caruselHuakin.style.width = (caruselHuakin.offsetWidth + 361) + "px";
+                    }
+                });
+                //конец ajax
+                
+                loadingBlock.style.display = "none";
+
+                //Кнопка аватара
+                var btnProfile = document.querySelector(".avatar");
+                btnProfile.onclick = function openProfile() {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open("get", "../Pages/modalPages/profile.html");
+                    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                    xhr.onreadystatechange = function () {
+                        if (xhr.readyState == 4 && xhr.status == 200) {
+                            modal.innerHTML = xhr.responseText;
+                            loadingBlock.style.display = "none";
+                            modal.style.display = "block"
+                            
+                            var btnCloseProfile = document.querySelector(".modal-close-btn");
+                            btnCloseProfile.onclick = function () {
+                                modal.style.display = "none";
+                            }
+                        }
+                    }
+                    xhr.send();
+                };
+
                 /////////////////////////////
                 //ОТКРЫТИЕ БОКОВЫХ ВКЛАДОК//
-                var 
+                var
                     btnProject = document.querySelector(".project"),
                     btnTask = document.querySelector(".task"),
                     btnOrganization = document.querySelector(".organization"),
-                    mainTaskBlock = document.querySelector(".main-task-block"),    
-                    mainOrganizationBlock = document.querySelector(".main-organization-block"),    
-                    mainProjectBlock = document.querySelector(".main-project-block");    
+                    mainTaskBlock = document.querySelector(".main-task-block"),
+                    mainOrganizationBlock = document.querySelector(".main-organization-block");
+                    
 
-                    // вкладка задачи
-                    btnTask.onclick = function () {
-                        mainOrganizationBlock.style.display = "none";
-                        mainProjectBlock.style.display = "none";
-                        mainTaskBlock.style.display = "block";
-                        btnProject.classList.remove("active");
-                        btnTask.classList.add("active");
-                        btnOrganization.classList.remove("active");
-                    }
-                    // вкладка организации
-                    btnOrganization.onclick = function () {
-                        mainProjectBlock.style.display = "none";
-                        mainTaskBlock.style.display = "none";
-                        mainOrganizationBlock.style.display = "block";
-                        btnProject.classList.remove("active");
-                        btnTask.classList.remove("active");
-                        btnOrganization.classList.add("active");
-                    }
-                    // вкладка проекты
-                    btnProject.onclick = function () {
-                        mainOrganizationBlock.style.display = "none";
-                        mainTaskBlock.style.display = "none";
-                        mainProjectBlock.style.display = "block";
-                        btnProject.classList.add("active");
-                        btnTask.classList.remove("active");
-                        btnOrganization.classList.remove("active");
-                    }
+                // вкладка задачи
+                btnTask.onclick = function () {
+                    mainOrganizationBlock.style.display = "none";
+                    mainProjectBlock.style.display = "none";
+                    mainTaskBlock.style.display = "block";
+                    btnProject.classList.remove("active");
+                    btnTask.classList.add("active");
+                    btnOrganization.classList.remove("active");
+                }
+                // вкладка организации
+                btnOrganization.onclick = function () {
+                    mainProjectBlock.style.display = "none";
+                    mainTaskBlock.style.display = "none";
+                    mainOrganizationBlock.style.display = "block";
+                    btnProject.classList.remove("active");
+                    btnTask.classList.remove("active");
+                    btnOrganization.classList.add("active");
+                }
+                // вкладка проекты
+                btnProject.onclick = function () {
+                    mainOrganizationBlock.style.display = "none";
+                    mainTaskBlock.style.display = "none";
+                    mainProjectBlock.style.display = "block";
+                    btnProject.classList.add("active");
+                    btnTask.classList.remove("active");
+                    btnOrganization.classList.remove("active");
+                }
                 //      КОНЕЦ ВКЛАДОК     //
                 ////////////////////////////
 
@@ -213,61 +325,34 @@ document.addEventListener("DOMContentLoaded", function () {
                 var btnLogin = document.querySelector('.autorization');
                 var login = document.querySelector('#login');
                 var password = document.querySelector('#password');
-                var checkbox = document.querySelector('.checkbox');
+                var checkbox = document.querySelector('.checkbox-remember');
                 
                 btnLogin.onclick = function () {
                     if (login.value === '' || password.value === '') {
                         showInputsError("Заполните поля", 3);
                     } else {
-                        xhr.open("POST", "http://tableco.ad-best.ru/php/login.php");
-                        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                        xhr.onreadystatechange = function () {
-                            if (xhr.readyState == 4 & xhr.status == 200) {
-                                if (xhr.responseText == 1) {
-                                    if (checkbox.checked == true) {
-                                        localStorage.setItem("login", login.value);
-                                        localStorage.setItem("password", password.value);
-                                    }
-                                    xhr.open("get", "../Pages/main.html");
-                                    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                                    xhr.onreadystatechange = function () {
-                                        if (xhr.readyState == 4 & xhr.status == 200) {
-                                            user.login = login.value;
-                                            user.password = password.value;
-                                            
-                                            mainView.innerHTML = xhr.responseText;
-                                            loadingBlock.style.display = "none";
-                                            mainHtml();
-                                        }
-                                    }
-                                    xhr.send();
-                                } else {
-                                    alert(xhr.responseText);
+                        ajax("POST", "http://tableco.ad-best.ru/php/login.php", true, 
+                             "login=" + login.value + "&password=" + password.value, function (e){
+                            if (e == 1) {
+                                if (checkbox.checked == true) {
+                                    localStorage.setItem("login", login.value);
+                                    localStorage.setItem("password", password.value);
                                 }
+                                ajax("GET", "../Pages/main.html", true, "", function(response) {
+                                    user.login = login.value;
+                                    user.password = password.value;
+                                    mainView.innerHTML = response;
+                                    mainHtml();
+                                });
                             }
-                        }
-                        xhr.send("login=" + login.value + "&password=" + password.value);
-//                        ajax("POST", "http://tableco.ad-best.ru/php/login.php", false, 
-//                             "login=" + login.value + "&password=" + password.value, function (){
-//                            if (xhr.responseText == 1) {
-//                                if (checkbox.checked == true) {
-//                                    localStorage.setItem("login", login.value);
-//                                    localStorage.setItem("password", password.value);
-//                                }
-//                                ajax("GET", "../Pages/main.html", false, "", function(response) {
-//                                    user.login = login.value;
-//                                    user.password = password.value;
-//                                    mainView.innerHTML = response;
-//                                    mainHtml();
-//                                });
-//                            }
-//                        });
+                        });
                     }
                 };
             }
         }
         xhr.send();
-    }
+    };
+
     //     КОНЕЦ ФУНКЦИИ   //
     /////////////////////////
 
@@ -303,5 +388,3 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     // конец localeStorage
 });
-
-//нажатие на регистрацию
